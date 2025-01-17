9c21105b768e2dc35507d52943864c8eea6f1cbd
saminnam <innam8264@gmail.com>
2025-01-17 10:23:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         "hero-pattern": "url('/src/assets/flight-hero1.png')",
         "card-pattern": "url('/src/assets/card-bg.png)",
         "footer-pattern": "url('src/assets/footer-bg.png')",
-        "common-hero-pattern": "url('src/assets/common-hero.png')",
+        "Global-hero-pattern": "url('src/assets/global-hero.png')",
         "download-bg-pattern": "url('src/assets/image.jpeg')",
         "status-bg-pattern": "url('src/assets/flight-bg.jpg')",
         "left-bg-pattern": "url('src/assets/left-bg.jpg')",
